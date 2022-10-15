0d02a4e12e485f36b4e9887036e149d3dbd4a4cc
atiradeon86 <atiradeon86@gmail.com>
2022-10-15 21:01:32 +0200

    Mod ...

--- mssql-adventureworks/startup.sh
+++ mssql-adventureworks/startup.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 echo "Get and Import DB"
+if [ ! -f /sample-db/AdventureWorks2019.bak ]; then
 mkdir /sample-db
 cd /sample-db
 wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak
 cd /
+fi
 /opt/mssql/bin/sqlservr & /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i db_install.sql  
 sleep 1d
