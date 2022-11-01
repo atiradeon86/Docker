c043a281d6274e6f4151dd66cbfc1f06643661df
Attila Horváth <atiradeon86@gmail.com>
2022-11-01 21:17:09 +0100

    sqlagent.enabled true

--- mssql-adventureworks/startup.sh
+++ mssql-adventureworks/startup.sh
@@ -6,5 +6,5 @@ cd /sample-db
 wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak
 cd /
 fi
-/opt/mssql/bin/sqlservr & /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i db_install.sql  
+/opt/mssql/bin/sqlservr & /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i db_install.sql
 sleep 1d
