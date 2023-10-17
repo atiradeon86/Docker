fb98476baea6e376058e61438bb037f73b9ef755
Attila Horváth <atiradeon86@gmail.com>
2023-10-17 19:55:16 +0200

    Import fix

--- mssql-adventureworks/db_install.sql
+++ mssql-adventureworks/db_install.sql
@@ -1,6 +1,6 @@
 USE [master]
-RESTORE DATABASE [AdventureWorks] 
+RESTORE DATABASE [AdventureWorks2019] 
     FROM DISK = '/sample-db/AdventureWorks2019.bak'
-        WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf',
-        MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks2019_log.ldf'
+        WITH MOVE 'AdventureWorks2019' TO '/var/opt/mssql/data/AdventureWorks2019.mdf',
+        MOVE 'AdventureWorks2019_log' TO '/var/opt/mssql/data/AdventureWorks2019_log.ldf'
 GO
